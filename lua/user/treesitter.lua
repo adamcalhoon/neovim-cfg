44ff0b5c90bdcc892f50dafebf6fded24bde4be3
Adam Calhoon <adam.calhoon@ready-robotics.com>
2022-10-27 22:14:18 -0400

    Disable C and C++ indent for treesitter bug

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -2,12 +2,9 @@ local status_ok, configs = pcall(require, "nvim-treesitter.configs")
 if not status_ok then return end
 
 configs.setup({
-    ensure_installed = "all", -- one of "all" or a list of languages
-    ignore_install = {""}, -- List of parsers to ignore installing
-    highlight = {
-        enable = true, -- false will disable the whole extension
-        disable = {"css"} -- list of language that will be disabled
-    },
+    ensure_installed = "all",
+    ignore_install = {""},
+    highlight = {enable = true, disable = {"css"}},
     autopairs = {enable = true},
-    indent = {enable = true, disable = {"python", "css"}}
+    indent = {enable = true, disable = {"c", "cpp", "css", "python"}}
 })
